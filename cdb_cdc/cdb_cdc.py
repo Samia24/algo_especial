73daaa6eea7e09de476184f41b692f198c55675c
Sâmia Braga <90200730+Samia24@users.noreply.github.com>
2023-08-22 00:26:11 -0300

    Add files via upload

--- cdb_cdc/cdb_cdc.py
+++ cdb_cdc/cdb_cdc.py
@@ -1,46 +1,55 @@
+def calcular_juros_cdb(principal, taxa, anos):
+    return principal * (1 + taxa/100)**anos
 
-def calcular_tempo_ano(ano_vencimento):
-    tempo_investimento = ano_vencimento - 2023
-    return tempo_investimento
+def calcular_juros_cdc(principal, taxa, meses):
+    return principal * (1 + taxa/100)**meses
 
-def juros_compostos(capital, taxa, tempo):
-    return capital * ((1 + (taxa / 100)) ** tempo)
+def calcular_lucro_banco_cdb(valor_cdb, taxa_cdb, anos_cdb, taxa_cdc, meses_cdc):
+    juros_cdb = calcular_juros_cdb(valor_cdb, taxa_cdb, anos_cdb)
+    juros_cdc = calcular_juros_cdc(valor_cdb, taxa_cdc, meses_cdc)
+    return juros_cdc - juros_cdb
+
+def calcular_lucro_banco_cdc(valor_cdc, taxa_cdb, anos_cdb, taxa_cdc, meses_cdc):
+    juros_cdb = calcular_juros_cdb(valor_cdc, taxa_cdb, anos_cdb)
+    juros_cdc = calcular_juros_cdc(valor_cdc, taxa_cdc, meses_cdc)
+    return juros_cdc - juros_cdb
+
 
 def main():
-    print('>>>>> Bem-vindo(a) ao Simulador Bancário! <<<<<')
+    print("\n--------- Simulador de Investimento em CDB ---------")
+    valor_investido = float(input("Informe o valor do investimento em CDB: R$ "))
+    taxa_cdb = float(input("Informe a taxa de juros anual do CDB (entre 1% e 20%): "))
+    anos_cdb = int(input("Informe o ano de vencimento (AAAA): "))
 
-    valor_investido = float(input('Informe o valor investido: R$ '))
-    taxa_cdb = float(input('Informe a taxa de juros do CDB (entre 1% e 20% ao ano): '))
-    ano_vencimento = int(input('Informe período de vencimento em anos (AAAA): '))
-
-    tempo_ano = calcular_tempo_ano(ano_vencimento) 
-
-    montante_cdb = juros_compostos(valor_investido, taxa_cdb, tempo_ano)
-    juros_recebidos = montante_cdb - valor_investido
+    tempo_anos = anos_cdb - 2023
     
-    valor_total_a_sacar = valor_investido + juros_recebidos
-    percentual_de_ganho = taxa_cdb * tempo_ano
+    montante_cdb = calcular_juros_cdb(valor_investido, taxa_cdb, tempo_anos)
+    rendimento_cdb = montante_cdb - valor_investido
 
-    print('>>>>>>>>>> Resultado da Simulação de CDB <<<<<<<<<<')
-    print(f'¬¬¬ Valor investido: R${valor_investido:.2f}')
-    print(f'¬¬¬ Juros Recebidos: R${juros_recebidos:.2f}')
-    print(f'¬¬¬ Total a sacar  : R${valor_total_a_sacar:.2f}')
-    print(f'¬¬¬ Rendimento     : {percentual_de_ganho:.1f}%')
-    print('\n-------------------------------')
+    print(f"\n>>>>>>>>> RESULTADO CDB <<<<<<<<<<")
+    print(f"O valor a ser pago ao investidor ao final do período é: R$ {montante_cdb:.2f}")
+    print(f"O rendimento do CDB é: R$ {rendimento_cdb:.2f}")
 
-    valor_emprestado = float(input('Informe o valor do empréstimo: R$ '))
-    taxa_cdc = float(input('Informe o valor da taxa de juros mensal (% -> entre 1 e 17%): ')) / 100
-    prazo_meses = int(input('Informe o prazo do empréstimo em meses (24x, 36x ou 60x): '))
-
-    parcela = juros_compostos(valor_emprestado, taxa_cdc, prazo_meses)
-    total_pago = parcela * prazo_meses
-    juros_totais = total_pago - valor_emprestado
+    print("\n--------- Simulador de Empréstimo CDC ---------")
+    valor_emprestimo = float(input("Informe o valor do empréstimo CDC: R$ "))
+    taxa_cdc = float(input("Informe a taxa de juros mensal do CDC (entre 1% e 17%): "))
+    meses_cdc = int(input("Informe o prazo do empréstimo em meses (24x, 36x ou 60x): "))
     
-    print('\n>>>>>>>>>> Resultado da Simulação de CDC <<<<<<<<<<')
-    print(f'¬¬¬ Valor total pago do Empréstimo ({prazo_meses}x): R$ {total_pago:.2f}')
-    print(f'¬¬¬ Total de juros pagos ({prazo_meses}x)          : R$ {juros_totais:.2f}')
-    print(f'¬¬¬ Valor da parcela mensal ({prazo_meses}x)       : R$ {parcela:.2f}')
-    print('-----------------------------')
+    juros_total_cdc = calcular_juros_cdc(valor_emprestimo, taxa_cdc, meses_cdc)
+    valor_total_cdc = valor_emprestimo + juros_total_cdc
+    valor_parcelas = juros_total_cdc / meses_cdc
+    
+    print(f"\n>>>>>>>>> RESULTADO CDC <<<<<<<<<<")
+    print(f"Valor total a ser pago pelo cliente no CDC: R$ {valor_total_cdc:.2f}")
+    print(f"O valor total de juros a ser pago pelo cliente é: R$ {juros_total_cdc:.2f}")
+    print(f"O valor das parcelas mensais é: R$ {valor_parcelas:.2f}")
 
+
+    lucro_banco_cdb = calcular_lucro_banco_cdb(montante_cdb, taxa_cdb, tempo_anos, taxa_cdc, meses_cdc)
+    lucro_banco_cdc = calcular_lucro_banco_cdc(valor_emprestimo, taxa_cdb, tempo_anos, taxa_cdc, meses_cdc)
+
+    print(f"\n>>>>>>>>> LUCROS <<<<<<<<<<")
+    print(f"Lucro do banco com investimento em CDB: R$ {lucro_banco_cdb:.2f}")
+    print(f"Lucro do banco com empréstimo CDC: R$ {lucro_banco_cdc:.2f}")
 
 main()
